a1689eeb23787aa45ad2fee2e5c5af26b6069f53
zarin-anjum <zarinanjum0801@gmail.com>
2025-09-16 22:34:55 +0600

    completed lab 5

---
+++ C241469_Lab_05.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+void merge(int arr[], int l, int m, int r){
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for(int i=0; i<n1; i++){
+        L[i] = arr[l + i];
+    }
+    for(int j=0; j<n2; j++){
+        R[j] = arr[m + 1 + j];
+    }
+
+    int i = 0, j = 0, k = l;
+    while(i < n1 && j < n2){
+        if(L[i] <= R[j]){
+            arr[k] = L[i];
+            i++;
+        }else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < n1){
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    while(j < n2){
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r){
+    if(l < r){
+        int mid = l + (r - l) / 2;
+
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+
+        merge(arr, l, mid, r);
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++){
+       cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
